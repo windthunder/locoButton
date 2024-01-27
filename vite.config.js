ae44a3008fcf9695a77aebb20f2de0dcba78fa80
anohito <windthunder310@gmail.com>
2024-01-27 21:03:26 +0800

    fix github page error

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig(({ command, mode, isSsrBuild, isPreview }) => {
+  if (command === 'serve') {
+    return {
+    }
+  } else {
+    // command === 'build'
+    return {
+      base: '/locoButton/'
+    }
+  }
+})

44af9f22811b123dbd3dd6978604902e65c9fdc5
anohito <windthunder310@gmail.com>
2024-01-28 13:22:06 +0800

    調整版型 加上音量控制

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,33 @@
 import './style.sass'
+import GUI from 'lil-gui';
 
-function getMp3Url(fileName: string){
-  const imgUrl = new URL(`../public/mp3/${fileName}`, import.meta.url).href
-  return imgUrl
+interface AudioButtonElement extends HTMLElement {
+  audio: HTMLAudioElement;
 }
+
+const gui = new GUI();
+
+const setting = {
+	volume: 1,
+};
+
+gui.add(setting, 'volume', 0, 1, 0.01);
+
+function getMp3Url(fileName: string) : string {
+  return new URL(`../public/mp3/${fileName}`, import.meta.url).href
+}
+
+// perload all mp3
+document.querySelectorAll('.sound').forEach((el) => {
+  const sound = (<AudioButtonElement>el).dataset.sound as string
+  (<AudioButtonElement>el).audio = new Audio(getMp3Url(sound))
+})
 
 // 當點擊.sound時 撥放 data-sound的音檔
 document.querySelectorAll('.sound').forEach((el) => {
-  el.addEventListener('click', () => {
-    const sound = el.getAttribute('data-sound') as string
-    const audio = new Audio(getMp3Url(sound))
+  (<AudioButtonElement>el).addEventListener('click', () => {
+    let audio = (<AudioButtonElement>el).audio
+    audio.volume = setting.volume
     audio.play()
   })
 })
